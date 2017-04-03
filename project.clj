5e4a2fb2b46cd1dea8eb4edf7b38568fe851cc9d
Antony Woods <antony@mastodonc.com>
2017-04-03 14:34:12 +0100

    Add nippy support

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [cheshire "5.7.0"]]
+                 [cheshire "5.7.0"]
+                 [com.taoensso/nippy "2.13.0"]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
   :repositories [["releases" {:url "https://clojars.org/repo"
