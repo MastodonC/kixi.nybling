7331e987e37605a9ccc76ba0b3eca3b62ca60d47
Tom Coupland <tcoupland@gmail.com>
2017-04-12 13:54:37 +0100

    Version 0.1.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kixi/kixi.nybling "0.1.4"
+(defproject kixi/kixi.nybling "0.1.5-SNAPSHOT"
   :description "A tiny library that converts formats"
   :url "http://github.com/mastodonc/kixi.nybling"
   :license {:name "Eclipse Public License"
